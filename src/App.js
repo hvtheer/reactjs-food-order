4ab763f52e1cbd684d612fbbadd922bec5a86dd7
The Hoang <hthe1010@gmail.com>
2022-08-17 17:11:31 +0700

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
+import Cart from './components/Cart'
 
 function App() {
   return (
     <CartProvider>
+      <Cart />
       <Header />
       <main>
         <Meals />
